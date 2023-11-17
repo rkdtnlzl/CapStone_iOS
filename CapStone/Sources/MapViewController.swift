4dd1e75561b72fdf2b746354d8236fb334da9235
rkdtnlzl <rkdtnlzl@naver.com>
2023-11-17 15:10:29 +0900

    [Feat]MapVC 생성 및 ClientID 추가

---
+++ CapStone/Sources/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  CapStone
+//
+//  Created by 강석호 on 2023/11/17.
+//
+
+//import Foundation
+//import NMapsMap
+//
+//class MapViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let naverMapView = NMFNaverMapView(frame: view.frame)
+//        view.addSubview(naverMapView)
+//    }
+//}

abdf893b3ef42752de1cf31276223792e72d26ee
rkdtnlzl <rkdtnlzl@naver.com>
2023-11-22 16:15:46 +0900

    [Feat] MapVC UI #2

--- CapStone/Sources/MapViewController.swift
+++ CapStone/Sources/MapViewController.swift
@@ -12,7 +12,19 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         let naverMapView = NMFNaverMapView(frame: view.frame)
         view.addSubview(naverMapView)
+        
+        self.navigationItem.title = "미즈 피부과"
+        
+        self.navigationController?.navigationBar.topItem?.title = ""
+
+        naverMapView.snp.makeConstraints { make in
+            make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
+            make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
 }

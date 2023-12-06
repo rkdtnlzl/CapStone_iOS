638a1e65555bd463a3e0f1b562cd6295ae8c3d18
rkdtnlzl <rkdtnlzl@naver.com>
2023-12-06 14:43:00 +0900

    [Fix] 폴더링 수정

---
+++ CapStone/Sources/Models/LearningModel.swift
@@ -0,0 +1,23 @@
+//
+//  LearningModel.swift
+//  CapStone
+//
+//  Created by 강석호 on 12/6/23.
+//
+
+import Foundation
+
+struct DetailCafeMenuResponse: Codable {
+    let code: Int
+    let result: String
+    let message: String
+    let data: [DetailCafeMenu]
+}
+
+struct DetailCafeMenu: Codable {
+    let menuId: Int
+    let name: String
+    let description: String
+    let price: Int
+    let imageUrl: String
+}
